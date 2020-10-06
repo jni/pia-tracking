e355d3d82fe339009a6636bc321c66480018554d
AbigailMcGovern <abigail_mcgovern@hotmail.com>
2020-10-07 00:52:32 +1100

    get_tracks working with new api + docstring

--- view_tracks.py
+++ view_tracks.py
@@ -36,18 +36,47 @@ def get_stack(data_path, object_channel=2, frame=70):
 
 
 def get_tracks(df, min_frames=20, id_col='particle', time_col='frame',
-        coord_cols=('x', 'y', 'z'), log=False, scale=(1, 1, 1)):
+        coord_cols=('x', 'y', 'z'), scale=(1, 1, 1)):
     """
-    Get the tracks for napari tracks. Will need to be updated before 
-    use with recent commits to the tracks PR (ID + data)
+    Get the tracks from pandas.DataFrame containing object ID, 
+    time, and coordinates for viewing in napari. Filters tracks
+    according to a specified or default minimum number of frames 
+    in which they should appear. Returns tuple containing an
+    array with columns ID, t, <coord cols> and the properties
+    dictionary.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        tracks data
+    min_frames: int
+        minimum frames in which an object should appear
+        to be added to the tracks data for viewing.
+    id_col: str
+        The name of the column in which object ID is found
+    time_col: str
+        The name of the column in which the time is found
+    coord_cols: tuple or list of str
+        names of columns containing the object coordinates
+    scale: numeric or tuple or string of numeric
+        To scale coordinates for viewing with the data
+    
+    Returns
+    -------
+    track_data: np.ndarray
+        array with cols ID, t, <coords>
+        The data is sorted by ID then t in accordance
+        with napari tracks data validation
+    dict(df_filtered): dict
+        dict containing the properties for napari
     """
     time_0 = time.time()
-    num_cols = len(coord_cols) + 2
     id_array = df[id_col].to_numpy()
     track_count = np.bincount(id_array)
     df['track length'] = track_count[id_array]
     df_filtered = df.loc[df['track length'] >= min_frames, :]
-    data_cols = [id_col, time_col] + coord_cols
+    df_filtered = df_filtered.sort_values(by=[id_col, time_col])
+    data_cols = [id_col, time_col] + list(coord_cols)
     track_data = df_filtered[data_cols].to_numpy()
     track_data[:, -3:] *= scale
     print(f'{np.sum(track_count >= min_frames)} tracks found in '
@@ -55,17 +84,11 @@ def get_tracks(df, min_frames=20, id_col='particle', time_col='frame',
     return track_data, dict(df_filtered)
 
 
-def save_tracks(tracks, name='tracks-for-napari.txt'):
+def save_tracks(tracks, name='tracks-for-napari.csv'):
     '''
-    For saving tracks in old napari tracks format
-    Not useful once changed to ID + data
+    For saving tracks into a csv file
     '''
-    output = []
-    for track in tracks:
-        ls = track.tolist()
-        output.append(ls)
-    with open(name, 'w+') as f:
-        f.write(str(output))
+    np.savetxt(name, tracks, delimiter=',')
 
 
 if __name__ == '__main__':
@@ -100,10 +123,11 @@ if __name__ == '__main__':
     # --------
     arr = get_stack(paths['data_path'])
     df = pd.read_csv(paths['tracks_path'])
-    if args.min_frames:
-        tracks = get_tracks(df, scale=[1, 1, 4], min_frames=args.min_frames)
+    if args.min_frames: 
+        tracks, properties = get_tracks(df, scale=[1, 1, 4], 
+                                        min_frames=args.min_frames)
     else:
-        tracks = get_tracks(df, scale=[1, 1, 4])
+        tracks, properties = get_tracks(df, scale=[1, 1, 4])
     # save_path = '/Users/amcg0011/GitRepos/pia-tracking/20200918-130313/tracks-for-napari.txt'
     # save_tracks(tracks, save_path)
 
