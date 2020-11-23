e50640241c24d68159141ba0d9590f5d9b95af98
AbigailMcGovern <abigail_mcgovern@hotmail.com>
2020-11-23 11:41:57 +1100

    support toggel modes and edit WIP frame

--- correct_labels.py
+++ correct_labels.py
@@ -14,7 +14,7 @@ def correct_labels(image_path, labels_path, frame, scale=(1,1,4), c=2):
     Correct labels to create a ground truth with four opperations:
         (1) points - to seed watershed to split labels.
         (2) watershed - recompute watershed for joined labels based on 
-            chosen points.
+            chosen point
         (3) pick label colour - choose the colour of the label with which
             to paint.
         (4) paint - paint new label. 
@@ -27,46 +27,56 @@ def correct_labels(image_path, labels_path, frame, scale=(1,1,4), c=2):
 class CorrectLabels:
     def __init__(self, image_path, labels_path, frame, scale=(1,1,4), c=2):
         """
-        Correct labels to create a ground truth with four opperations:
-            (1) points - to seed watershed to split labels.
+        Correct labels to create a ground truth with five opperations, each of which 
+        correspond to the following number key. 
+            (1) toggel selection of points - to seed watershed to split labels.
             (2) watershed - recompute watershed for joined labels based on 
                 chosen points.
-            (3) pick label colour - choose the colour of the label with which
+            (3) toggle pick label colour - choose the colour of the label with which
                 to paint.
-            (4) paint - paint new label. 
+            (4) toggle label fill mode - for merging labels
+            (5) toggel paint mode - paint new label. 
+        Output can be saved by using Control-s 
         """
         self.scale = scale
         # Read in data
         self.array = single_zarr(image_path, c=c)[frame].compute()
         if labels_path.endswith('_GT.zarr'):
-            self.labels = zarr.open(labels_path, mode='r+')
+            self.labels = np.array(zarr.open(labels_path, mode='r+'))
+            # save path is the labels path
+            self.save_path = labels_path
         else:
             self.labels = zarr.open(labels_path, mode='r+')[frame]
-        # Get the save path 
-        data_path = Path(labels_path)
-        self.save_path = os.path.join(data_path.parents[0], data_path.stem + f'_t{frame}_GT.zarr')
-        # Visualise and annotate
+            # Get the save path 
+            data_path = Path(labels_path)
+            self.save_path = os.path.join(data_path.parents[0], 
+                                          data_path.stem + f'_t{frame}_GT.zarr')
 
 
     def __call__(self):
         with napari.gui_qt():
             v = napari.Viewer()
+            print(type(self.labels))
             v.add_image(self.array, name='Image', scale=self.scale)
             v.add_labels(self.labels, name='Labels', scale=self.scale)
             v.add_points(np.empty((0, len(self.labels.shape)), dtype=float), scale=self.scale, size=2)
             v.bind_key('1', self._points)
             v.bind_key('2', self._watershed)
             v.bind_key('3', self._select_colour)
-            v.bind_key('4', self._paint)
-            v.bind_key('s', self._save)
+            v.bind_key('4', self._fill)
+            v.bind_key('5', self._paint)
+            v.bind_key('Shift-s', self._save)
     
 
     def _points(self, viewer):
         """
         Switch to points layer to split a label
         """
-        viewer.layers['Points'].mode = 'add'    
-    
+        if viewer.layers['Points'].mode != 'add':  
+            viewer.layers['Points'].mode = 'add' 
+        else:   
+            viewer.layers['Points'].mode = 'pan_zoom' 
+
 
     def _watershed(self, viewer):
         """
@@ -76,7 +86,6 @@ class CorrectLabels:
         # find the labels corresponding to the current points in the points layer
         labels = viewer.layers['Labels'].data
         image = viewer.layers['Image'].data
-        print(type(labels))
         points = viewer.layers['Points'].data
         points = np.round(points).astype(int)
         labels = watershed_split(image, labels, points)
@@ -89,14 +98,30 @@ class CorrectLabels:
         """
         Select colour for painting
         """
-        viewer.layers['Labels'].mode = 'pick'    
+        if viewer.layers['Labels'].mode != 'pick': 
+            viewer.layers['Labels'].mode = 'pick'  
+        else:
+            viewer.layers['Labels'].mode = 'pan_zoom'  
     
+
+    def _fill(self, viewer):
+        """
+        Switch napari labels layer to fill mode
+        """
+        if viewer.layers['Labels'].mode != 'fill':
+            viewer.layers['Labels'].mode = 'fill' 
+        else:
+            viewer.layers['Labels'].mode = 'pan_zoom'
+
 
     def _paint(self, viewer):
         """
         Switch napari labels layer to paint mode
         """
-        viewer.layers['Labels'].mode = 'paint'    
+        if viewer.layers['Labels'].mode != 'paint':  
+            viewer.layers['Labels'].mode = 'paint'  
+        else:  
+            viewer.layers['Labels'].mode = 'pan_zoom'
     
 
     def _save(self, viewer): 
